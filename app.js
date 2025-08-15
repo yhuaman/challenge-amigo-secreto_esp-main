db0326bbddea518e99a534e0cc865a9eba8327e8
Natali Huaman Rojas <natalihr.3@gmail.com>
2025-08-15 16:44:42 -0500

    Creación de las funciones

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function xAgregarAmigo(){
+
+    let nombre = document.getElementById("nomAmigo").value.trim();
+    if(nombre === ""){
+        alert("El sistema no permite valores nulos, ingrese un nombre valido");
+        return;
+    }
+
+    amigos.push(nombre);
+    document.getElementById("nomAmigo").value = "";
+    xMostrarLista();
+
+}
+
+function xMostrarLista(){
+
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    })
+
+}
+
+function xSortearAmigo(){
+    if(amigos.length === 0){
+        alert("La lista de amigos se encuenta vacia, Agregar los nombres primero");
+        return;
+    }
+
+    let xIndiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let xAmigoSecreto = amigos[xIndiceAleatorio];
+    document.getElementById("resultado").textContent = "El amigo secreto es: " + xAmigoSecreto;
+}
+
